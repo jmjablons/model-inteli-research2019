958b40b83a16993ff1c86123a0a86998bb697192
malicious bunny <juda.jablonska@gmail.com>
2020-08-03 14:20:38 +0200

    decide for the best model #check2

--- figure_source/parametermodel_fig8.R
+++ figure_source/parametermodel_fig8.R
@@ -14,14 +14,62 @@ tplot <- (p1 + p2) / (p3 + p4) + plot_annotation(tag_levels = "A")
 ggsave("fig/czerwiec2020/fig8.pdf", tplot, device = cairo_pdf,  
        scale = 1.3, width = 80, height = 100, units = "mm")
 
-remodel[["fictitious"]] %>%
-  left_join(manimal) %>%
-  select("par.alpha", substance) %>%
-  filter(par.alpha > .6)
+# hybrid + q-decay+ revision ----------------------------------------------
 
-remodel[["fictitious"]] %>%
+tplot <- remodel[[4]] %>%
+  select(tag, par.alpha.pos = par.beta, name) %>%
+  rbind(
+    remodel[[12]] %>%
+      select(tag, par.alpha.pos = par.beta, name)) %>% 
+  tidyr::spread(name, par.alpha.pos) %>%
+  mutate(dif = hybrid - `q-decay+`) %>%
+  tidyr::gather(param, value, -tag) %>%
   left_join(manimal) %>%
-  select("par.beta", substance) %>%
-  filter(par.beta > 12)
+  filter(param %in% c("hybrid", "q-decay+")) %>%
+  ggplot(aes(x = param, y = value, group = tag)) +
+  geom_line(size = .1)+
+  geom_jitter(width = .1, pch = 21, fill = "gray")+
+  geom_hline(yintercept = 0, colour = "gray")+
+  scale_y_continuous(limits = c(-15, 15), expand = c(0,0), breaks = c(-10, 0, 10))+
+  facet_wrap(~substance)+
+  labs(y = "par.beta")
 
-remodel[["q-decay+"]]
+tplot <- remodel[[4]] %>%
+  select(tag, par.alpha.pos = par.alpha.neg, name) %>%
+  rbind(
+    remodel[[12]] %>%
+      select(tag, par.alpha.pos = par.storage.pos, name)) %>% 
+  tidyr::spread(name, par.alpha.pos) %>%
+  mutate(dif = hybrid - `q-decay+`) %>%
+  select(tag, value = dif) %>%
+  left_join(manimal) %>%
+  ggplot(aes(x = substance, y = value)) +
+  box_default() + point_default()+
+  scale_y_continuous(limits = c(-1, 1), expand = c(0,0), breaks = c(-1, 0 ,1))+
+  labs(y = "diff par.alpha-")
+
+tplot <- remodel[[4]] %>%
+  select(tag, par.alpha.pos = par.beta, name) %>%
+  rbind(
+    remodel[[12]] %>%
+      select(tag, par.alpha.pos = par.beta, name)) %>% 
+  tidyr::spread(name, par.alpha.pos) %>%
+  mutate(dif = hybrid - `q-decay+`) %>%
+  select(tag, value = dif) %>%
+  left_join(manimal) %>%
+  ggplot(aes(x = substance, y = value)) +
+  box_default() + point_default()+
+  scale_y_continuous(limits = c(-15, 15), expand = c(0,0), breaks = c(-15, 0 ,15))+
+  labs(y = "diff par.beta")
+
+tplot <- remodel[[12]] %>%
+  select(tag, value = par.storage.neg, name) %>% 
+  left_join(manimal) %>%
+  ggplot(aes(x = substance, y = value)) +
+  box_default() + point_default()+
+  scale_y_continuous(limits = c(0, 1), expand = c(.1,0), breaks = c(0, .1, .5, 1))+
+  labs(y = "par.storage - q-decay+")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave("./wyniki_20200803/param_storage.pdf", tplot, device = cairo_pdf,  
+       scale = 1, width = 80, height = 80, units = "mm")
